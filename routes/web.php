e4225865aec993e564dd3d3db5c0d715ac27dd02
Cesar 8a <cesar8a.upiicsa@gmail.com>
2023-12-21 01:34:12 -0600

    Ajuste en el archivo de rutas, con ejemplo de nuevas rutas y test para probar las mismas

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return '<h1>Home</h1>';
+});
+
+Route::get('/cochoa', function () {
+	return '<h1>Cochoa</h1>';
+});
+
+Route::get('/usuarios', function () {
+	return '<h1>Usuarios</h1>';
+});
+
+Route::get('/usuarios/{id}', function ($id) {
+	return "<h1>Mostrando detalle del usuario: {$id}</h1>";
+})->where('id', '[0-9]+');
+
+Route::get('/usuarios/nuevo', function () {
+	return '<h1>Crear nuevo usuario</h1>';
+});
+
+Route::get('/saludo/{name}/{nickname?}', function ($name, $nickname = null) {
+	$name = ucfirst($name);
+
+	if ($nickname) {
+		return "<h1>Bienvenido {$name}, tu apodo es {$nickname}</h1>";
+	}else{
+		return "<h1>Bienvenido {$name}<h1>";
+	}
 });
 
 Route::get('/algoritmos-tema-1', 'App\Http\Controllers\algorithmsController@index');
